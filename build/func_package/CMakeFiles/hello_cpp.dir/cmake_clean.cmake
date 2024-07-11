6040b283de985908dcf13ac75f27c17e03b3e1f2
vboxuser <vboxuser@ROSnoetic.myguest.virtualbox.org>
2024-07-11 18:23:18 +0800

    add

---
+++ build/func_package/CMakeFiles/hello_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/vboxuser/demo_2/devel/lib/func_package/hello_cpp"
+  "/home/vboxuser/demo_2/devel/lib/func_package/hello_cpp.pdb"
+  "CMakeFiles/hello_cpp.dir/src/hello_cpp.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/func_package/catkin_generated/installspace/hi_py.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import rospy
+
+if __name__=='__main__':
+    rospy.INFO("py start~~")
+    rospy.init_node("set node ~!!!")
+    rospy.INFO("py end ~~")
