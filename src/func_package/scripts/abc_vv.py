f9a319c6e54e758a2d012b1b78c06fc15d7d6149
CCodeInspect <302869907@qq.com>
2024-07-14 20:50:06 +0800

    提交

---
+++ src/func_package/scripts/abc_vv.py
@@ -0,0 +1,3 @@
+#! /usr/bin/env python3
+
+num_bb = 100
--- src/func_package/scripts/get_log.py
+++ src/func_package/scripts/get_log.py
@@ -1,4 +1,10 @@
-import rospy
+import rospy, os, sys
+
+
+# path = os.path.abspath(".")
+sys.path.insert(0, "/home/vboxuser/demo_2/src/func_package/scripts")
+
+import abc_vv
 
 
 def get_log():
@@ -7,6 +13,7 @@ def get_log():
     rospy.logwarn("warn.........")
     rospy.logerr("err............")
     rospy.logfatal("fatal.........")
+    rospy.loginfo("num_bb: %d", num_bb)
 
 
 if __name__ == "__main__":
